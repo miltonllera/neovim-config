4ccf36b5daca19495fe28d95b22330bec34c1d52
carlos-gil <carlos.gil@hp.com>
2022-10-24 08:58:58 -0400

    fixed issue with jsx and rainbow

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -14,7 +14,7 @@ ts.setup {
   rainbow = {
     enable = true,
     disable = { "jsx", "tsx", "cpp" }, --list of languages you want to disable the plugin for
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
     -- colors = {}, -- table of hex strings
     -- termcolors = {} -- table of colour name strings
--- lua/themes.lua
+++ lua/themes.lua
@@ -41,7 +41,7 @@ require('material').setup({
     terminal = false, -- Enable contrast for the built-in terminal
     sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
     floating_windows = false, -- Enable contrast for floating windows
-    cursor_line = true, -- Enable darker background for the cursor line
+    cursor_line = false, -- Enable darker background for the cursor line
     non_current_windows = false, -- Enable darker background for non-current windows
     filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
   },
