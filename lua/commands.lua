c449ddeb24144b175573c534531ab17d5904e214
Milton Montero <m.lleramontero@bristol.ac.uk>
2023-04-21 22:23:06 +0100

    Update swap folder deletion.

--- lua/commands.lua
+++ lua/commands.lua
@@ -6,7 +6,7 @@ vim.cmd [[autocmd BufWritePre * %s/\s\+$//e ]]
 
 -- Swap folder
 vim.cmd('command! ListSwap split | enew | r !ls -l ~/.local/share/nvim/swap')
-vim.cmd('command! CleanSwap !rm -rf ~/.local/share/nvim/swap/')
+vim.cmd('command! CleanSwap !rm -rf ~/.local/state/nvim/swap/')
 
 -- Open help tags
 vim.cmd("command! HelpTags Telescope help_tags")
